3480c17f63d07aadee0d07e00437b60d4d51e948
Devante Lowery <dlowery2120@gmail.com>
2021-01-06 13:32:16 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -5,20 +5,5 @@ def first_challenge
 }
 end
 
-def second_challenge
-epic_tragedy = {
-   :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
-}
-end
+
 
